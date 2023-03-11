007ebbc3511c4c393da619fddab4ecfe3ce8e063
shella12 <ayeshaarshad4567@gmail.com>
2023-03-11 10:28:44 +0500

    Add Models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    belongs_to :post
+    before_save :comment_counter
+
+    def comment_counter
+        post.update(comments_counter: post.comments.all.length)
+      end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author_id
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+  before_save :post_count
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def post_count
+    author.update(posts_counter: author.posts.all.length)
+  end
+
 end
